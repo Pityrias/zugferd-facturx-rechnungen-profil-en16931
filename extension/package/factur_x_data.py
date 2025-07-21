3a908f56caadfebb77453a6f418169c7aedfcf36
Pityrias <neuglienicke.lui@t-online.de>
2025-07-21 17:38:42 +0200

    add _ to mark translatable strings

--- extension/package/factur_x_data.py
+++ extension/package/factur_x_data.py
@@ -16,6 +16,8 @@
 import uno
 import unohelper
 
+_ = gettext.gettext
+
 
 class ItemData:
 
@@ -127,16 +129,18 @@ class CategoryData:
 
 def check_position_data(position_data):
     if len(position_data) < 1:
-        return (False, "There must be at least one position listed")
+        return (False, _("There must be at least one position listed"))
     cnt = 1
     for pos in position_data:
         if not pos.is_valid():
-            return (False, f"Position {cnt} lacks required fields")
+            return (False, _(f"Position {cnt} lacks required fields"))
         calc_price = pos.netto_price * pos.amount
         if abs(calc_price - pos.netto_total) > 0.0001:
             return (
                 False,
-                f"Position {cnt}: netto total is not equal to netto price times the amount",
+                _(
+                    f"Position {cnt}: netto total is not equal to netto price times the amount"
+                ),
             )
         cnt += 1
     return (True, "")
@@ -144,16 +148,18 @@ def check_position_data(position_data):
 
 def check_category_data(category_data):
     if len(category_data) < 1:
-        return (False, "There must be at least one tax category listed")
+        return (False, _("There must be at least one tax category listed"))
     cnt = 1
     for category in category_data:
         if not category.is_valid():
-            return (False, f"Tax category {cnt} lacks required fields")
+            return (False, _(f"Tax category {cnt} lacks required fields"))
         calc_tax_total = category.rate / 100 * category.taxed_amount
         if abs(calc_tax_total - category.sum_tax) > 0.0001:
             return (
                 False,
-                f"Tax category {cnt}: sum tax is not equal rate percent of taxed amount",
+                _(
+                    f"Tax category {cnt}: sum tax is not equal rate percent of taxed amount"
+                ),
             )
         cnt += 1
     return (True, "")
